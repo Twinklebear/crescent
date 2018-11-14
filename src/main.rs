b99ff6ad1666efcde2b56c45044000e90c2d9e93
Will Usher <willusher.life@gmail.com>
2018-11-13 23:48:30 -0600

    files

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,26 @@
+#[macro_use]
+extern crate ispc;
+
+ispc_module!(crescent);
+
 fn main() {
-    println!("Hello, world!");
+    let width = 512;
+    let height = 512;
+    let mut framebuffer = vec![0.0; width * height * 3];
+    let mut srgb_img_buf = vec![0u8; width * height * 3];
+    // Can I do scene setup in Rust and build everything, then just
+    // hand off to ISPC for rendering?
+    unsafe {
+        crescent::render(std::ptr::null(), width as i32, height as i32,
+                         framebuffer.as_mut_ptr());
+        crescent::framebuffer_to_srgb(framebuffer.as_ptr(), srgb_img_buf.as_mut_ptr(),
+                                      width as i32, height as i32);
+    }
+
+    match image::save_buffer("out.png", &srgb_img_buf[..], width as u32, height as u32,
+                             image::RGB(8)) {
+        Ok(_) => println!("Rendered image saved to out.png"),
+        Err(e) => panic!("Error saving image: {}", e),
+    };
 }
+
