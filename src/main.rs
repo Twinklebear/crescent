7e593b62f79134b279f862fefcf6961b04117e4c
Will Usher <willusher.life@gmail.com>
2018-12-30 20:42:58 -0800

    now use tobj to load a mesh

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,9 @@
 extern crate ispc;
 extern crate sol;
 extern crate cgmath;
+extern crate tobj;
+
+use std::path::Path;
 
 use cgmath::{Vector3, Vector4};
 use sol::{Device, Geometry, IntersectContext, RayN, RayHitN, Scene, TriangleMesh};
@@ -13,25 +16,36 @@ fn main() {
     let height = 512;
     let mut framebuffer = vec![0.0; width * height * 3];
     let mut srgb_img_buf = vec![0u8; width * height * 3];
-    // Can I do scene setup in Rust and build everything, then just
-    // hand off to ISPC for rendering?
-    // TODO: Yes, the handles for embree are the same across everything
-    // so it's fine to setup everything in Rust and then pass
-    // the scene handle over to ISPC
     let device = Device::new();
 
-    // Make a triangle
-    let mut triangle = TriangleMesh::unanimated(&device, 1, 3);
-    {
-        let mut verts = triangle.vertex_buffer.map();
-        let mut tris = triangle.index_buffer.map();
-        verts[0] = Vector4::new(-1.0, 0.0, 0.0, 0.0);
-        verts[1] = Vector4::new(0.0, 1.0, 0.0, 0.0);
-        verts[2] = Vector4::new(1.0, 0.0, 0.0, 0.0);
+    let args: Vec<_> = std::env::args().collect();
+    let (models, _) = tobj::load_obj(&Path::new(&args[1])).unwrap();
+    let mesh = &models[0].mesh;
 
-        tris[0] = Vector3::new(0, 1, 2);
+    println!("Mesh has {} triangles and {} verts",
+             mesh.indices.len() / 3, mesh.positions.len() / 3);
+
+    // Make a triangle
+    let mut tris = TriangleMesh::unanimated(&device,
+                                            mesh.indices.len() / 3,
+                                            mesh.positions.len() / 3);
+    {
+        let mut verts = tris.vertex_buffer.map();
+        let mut tris = tris.index_buffer.map();
+        for i in 0..mesh.positions.len() / 3 { 
+            verts[i] = Vector4::new(mesh.positions[i * 3],
+                                    mesh.positions[i * 3 + 1],
+                                    mesh.positions[i * 3 + 2],
+                                    0.0);
+        }
+
+        for i in 0..mesh.indices.len() / 3 { 
+            tris[i] = Vector3::new(mesh.indices[i * 3],
+                                   mesh.indices[i * 3 + 1],
+                                   mesh.indices[i * 3 + 2]);
+        }
     }
-    let mut tri_geom = Geometry::Triangle(triangle);
+    let mut tri_geom = Geometry::Triangle(tris);
     tri_geom.commit();
 
     let mut scene = Scene::new(&device);
@@ -49,7 +63,7 @@ fn main() {
         for (i, mut ray) in rays.iter_mut().enumerate() {
             let x = (i as f32 + 0.5) / width as f32 - 0.5;
             let dir_len = f32::sqrt(x * x + y * y + 1.0);
-            ray.set_origin(Vector3::new(0.0, 0.5, 2.0));
+            ray.set_origin(Vector3::new(0.0, 0.0, 3.5));
             ray.set_dir(Vector3::new(x / dir_len, y / dir_len, -1.0 / dir_len));
         }
 
