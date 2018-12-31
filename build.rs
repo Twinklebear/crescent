55c4c6d9535a6ac650bef524a8a30b58a4b3f4e8
Will Usher <willusher.life@gmail.com>
2018-12-30 19:04:41 -0800

    link with embree, use sol to render the same triangle example

--- build.rs
+++ build.rs
@@ -1,5 +1,8 @@
 extern crate ispc;
 
+use std::env;
+use std::path::PathBuf;
+
 fn main() {
     let mut cfg = ispc::Config::new();
     let ispc_files = vec!["src/crescent.ispc"];
@@ -7,5 +10,16 @@ fn main() {
         cfg.file(*s);
     }
     cfg.compile("crescent");
+
+    if let Ok(e) = env::var("EMBREE_DIR") {
+        let mut embree_dir = PathBuf::from(e);
+        embree_dir.push("lib");
+        println!("cargo:rustc-link-search=native={}", embree_dir.display());
+    } else {
+        println!("cargo:error=Please set EMBREE_DIR=<path to embree3 root>");
+        panic!("Failed to find embree");
+    }
+    println!("cargo:rerun-if-env-changed=EMBREE_DIR");
+    println!("cargo:rustc-link-lib=embree3");
 }
 
--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,10 @@
 #[macro_use]
 extern crate ispc;
+extern crate sol;
+extern crate cgmath;
+
+use cgmath::{Vector3, Vector4};
+use sol::{Device, Geometry, IntersectContext, RayN, RayHitN, Scene, TriangleMesh};
 
 ispc_module!(crescent);
 
@@ -13,9 +18,52 @@ fn main() {
     // TODO: Yes, the handles for embree are the same across everything
     // so it's fine to setup everything in Rust and then pass
     // the scene handle over to ISPC
+    let device = Device::new();
+
+    // Make a triangle
+    let mut triangle = TriangleMesh::unanimated(&device, 1, 3);
+    {
+        let mut verts = triangle.vertex_buffer.map();
+        let mut tris = triangle.index_buffer.map();
+        verts[0] = Vector4::new(-1.0, 0.0, 0.0, 0.0);
+        verts[1] = Vector4::new(0.0, 1.0, 0.0, 0.0);
+        verts[2] = Vector4::new(1.0, 0.0, 0.0, 0.0);
+
+        tris[0] = Vector3::new(0, 1, 2);
+    }
+    let mut tri_geom = Geometry::Triangle(triangle);
+    tri_geom.commit();
+
+    let mut scene = Scene::new(&device);
+    scene.attach_geometry(tri_geom);
+    let rtscene = scene.commit();
+
+    let mut intersection_ctx = IntersectContext::coherent();
+
+    // Render the scene
+    for j in 0..height {
+        let y = -(j as f32 + 0.5) / height as f32 + 0.5;
+
+        // Try out streams of scanlines across x
+        let mut rays = RayN::new(width);
+        for (i, mut ray) in rays.iter_mut().enumerate() {
+            let x = (i as f32 + 0.5) / width as f32 - 0.5;
+            let dir_len = f32::sqrt(x * x + y * y + 1.0);
+            ray.set_origin(Vector3::new(0.0, 0.5, 2.0));
+            ray.set_dir(Vector3::new(x / dir_len, y / dir_len, -1.0 / dir_len));
+        }
+
+        let mut ray_hit = RayHitN::new(rays);
+        rtscene.intersect_stream_soa(&mut intersection_ctx, &mut ray_hit);
+        for (i, hit) in ray_hit.hit.iter().enumerate().filter(|(_i, h)| h.hit()) {
+            let uv = hit.uv();
+            framebuffer[(i + j * width) * 3] = uv.0;
+            framebuffer[(i + j * width) * 3 + 1] = uv.1;
+            framebuffer[(i + j * width) * 3 + 2] = 0.0;
+        }
+    }
+
     unsafe {
-        crescent::render(std::ptr::null(), width as i32, height as i32,
-                         framebuffer.as_mut_ptr());
         crescent::framebuffer_to_srgb(framebuffer.as_ptr(), srgb_img_buf.as_mut_ptr(),
                                       width as i32, height as i32);
     }
